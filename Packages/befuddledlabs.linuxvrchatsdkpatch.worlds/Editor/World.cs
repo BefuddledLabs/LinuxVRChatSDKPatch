273a2e1cb437777333846b94115e0c1446b616d3
DeltaNeverUsed <sinus06@hotmail.com>
2025-03-29 15:44:42 +0100

    chore!: removed support for sdk versions <3.6.6
    please use version 0.0.4 for sdk versions below 3.6.6

--- Packages/befuddledlabs.linuxvrchatsdkpatch.worlds/Editor/World.cs
+++ Packages/befuddledlabs.linuxvrchatsdkpatch.worlds/Editor/World.cs
@@ -1,13 +1,11 @@
 using System;
-using System.Collections.Generic;
 using System.Diagnostics;
 using System.IO;
-using System.Linq;
-using System.Reflection.Emit;
 using System.Text;
 using System.Text.RegularExpressions;
 using System.Threading;
 using HarmonyLib;
+using UnityEngine.Networking;
 using VRC.Core;
 using VRC.SDK3.Editor.Builder;
 using VRC.SDKBase.Editor;
@@ -19,8 +17,8 @@ namespace BefuddledLabs.LinuxVRChatSdkPatch.Worlds.Editor
     public static class World
     {
         [HarmonyPrefix]
-        [HarmonyPatch(typeof(VRCWorldAssetExporter), "RunScene", typeof(string), typeof(string))]
-        public static bool RunScenePrefix(object[] __args)
+        [HarmonyPatch(typeof(VRCWorldAssetExporter), "RunWorldTestDesktop", typeof(string), typeof(string))]
+        public static bool RunWorldTestDesktopPrefix(object[] __args)
         {
             var vrcInstallPath = SDKClientUtilities.GetSavedVRCInstallPath();
             if (string.IsNullOrEmpty(vrcInstallPath) || !File.Exists(vrcInstallPath))
@@ -39,13 +37,15 @@ namespace BefuddledLabs.LinuxVRChatSdkPatch.Worlds.Editor
             // Making sure that the paths are using forward slashes
             var bundleFilePath = ((string)__args[0]).Replace('\\', '/');
             var pluginFilePath = ((string)__args[1]).Replace('\\', '/');
+            
+            bundleFilePath = UnityWebRequest.EscapeURL(bundleFilePath).Replace("+", "%20");
+            pluginFilePath = UnityWebRequest.EscapeURL(pluginFilePath).Replace("+", "%20");
 
             var args = new StringBuilder();
             args.Append("run ");
             args.Append(vrcInstallPath);
             args.Append(' ');
             
-            args.Append('\'');
             args.Append("--url=create?roomId=");
                 args.Append(VRC.Tools.GetRandomDigits(10)); // Random roomId
                     args.Append("&hidden=true");
@@ -57,7 +57,6 @@ namespace BefuddledLabs.LinuxVRChatSdkPatch.Worlds.Editor
                                     args.Append("&pluginUrl=file:///");
                                     args.Append(pluginFilePath);
                                 }
-            args.Append('\'');
             
             args.Append(" --enable-debug-gui");
             args.Append(" --enable-sdk-log-levels");
@@ -66,8 +65,12 @@ namespace BefuddledLabs.LinuxVRChatSdkPatch.Worlds.Editor
                 args.Append(" --no-vr");
             if (VRCSettings.WatchWorlds)
                 args.Append(" --watch-worlds");
+            
 
             var argsPathFixed = Regex.Replace(args.ToString(), @"file:[/\\]*", "file:///Z:/"); // The file we have is relative to / and not the "c drive" Z:/ is /
+            
+            Debug.Log(Base.Editor.Base.GetSavedProtonPath() + argsPathFixed);
+
             var processStartInfo =
                 new ProcessStartInfo(Base.Editor.Base.GetSavedProtonPath(), argsPathFixed)
                 {
@@ -77,7 +80,6 @@ namespace BefuddledLabs.LinuxVRChatSdkPatch.Worlds.Editor
                         { "STEAM_COMPAT_CLIENT_INSTALL_PATH", Environment.GetEnvironmentVariable("HOME") + "/.steam/" }
                     },
                     WorkingDirectory = Path.GetDirectoryName(vrcInstallPath) ?? "",
-                    RedirectStandardOutput = true,
                     UseShellExecute = false
                 };
             for (var index = 0; index < VRCSettings.NumClients; ++index)
@@ -87,51 +89,6 @@ namespace BefuddledLabs.LinuxVRChatSdkPatch.Worlds.Editor
             }
 
             return false;
-        }
-
-        // Thanks Bartkk <3
-        [HarmonyTranspiler]
-        [HarmonyPatch(typeof(VRCWorldAssetExporter), "ExportCurrentSceneResource", typeof(bool), typeof(Action<string>), typeof(Action<object>))]
-        public static IEnumerable<CodeInstruction> ExportCurrentSceneResourceTranspiler(IEnumerable<CodeInstruction> instructions)
-        {
-            var code = instructions.ToList();
-
-            var insertionIndex = -1;
-            for (var i = 0; i < code.Count - 1; i++)
-            {
-                // strArray[4] = ".vrcw";
-                if (code[i].opcode != OpCodes.Ldstr) continue;
-                if (!code[i].operand.ToString().Equals(".vrcw")) continue;
-                
-                insertionIndex = i - 1;
-                break;
-            }
-
-            if (insertionIndex == -1)
-            {
-                Debug.LogError("Couldn't find place to modify ExportCurrentSceneResource");
-                return code;
-            }
-
-            var toAdd = new List<CodeInstruction>();
-
-            // ModifyArray(strArray);
-            toAdd.Add(new CodeInstruction(OpCodes.Dup));
-            toAdd.Add(new CodeInstruction(OpCodes.Call, AccessTools.Method(typeof(World), nameof(ModifyArray), new[] { typeof(string[]) })));
-
-            code.InsertRange(insertionIndex, toAdd);
-            return code;
-        }
-
-        static void ModifyArray(string[] arr)
-        {
-            // arr[1] = EditorUserBuildSettings.activeBuildTarget.ToString().ToLower();
-            for (var i = 0; i < arr.Length; i++)
-            {
-                if(arr[i] == null) continue;
-            
-                arr[i] = arr[i].ToLower();
-            }
         }
     }
 }
