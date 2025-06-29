719732247dbc09e29dda46f158e69a9da1a1f277
DeltaNeverUsed <sinus06@hotmail.com>
2025-06-29 16:21:07 +0200

    fix: 3.8.2 worlds changed signature of test method

--- Packages/befuddledlabs.linuxvrchatsdkpatch.worlds/Editor/World.cs
+++ Packages/befuddledlabs.linuxvrchatsdkpatch.worlds/Editor/World.cs
@@ -17,7 +17,7 @@ namespace BefuddledLabs.LinuxVRChatSdkPatch.Worlds.Editor
     public static class World
     {
         [HarmonyPrefix]
-        [HarmonyPatch(typeof(VRCWorldAssetExporter), "RunWorldTestDesktop", typeof(string), typeof(string))]
+        [HarmonyPatch(typeof(VRCWorldAssetExporter), "RunWorldTestDesktop", typeof(string))]
         public static bool RunWorldTestDesktopPrefix(object[] __args)
         {
             var vrcInstallPath = SDKClientUtilities.GetSavedVRCInstallPath();
@@ -26,7 +26,7 @@ namespace BefuddledLabs.LinuxVRChatSdkPatch.Worlds.Editor
                 Debug.LogError("couldn't get VRChat path..");
                 return true;
             }
-            
+
             var compatDataPath = Base.Editor.Base.GetVrcCompatDataPath();
             if (compatDataPath == null) // Check if we could find the compatdata directory
             {
@@ -36,28 +36,21 @@ namespace BefuddledLabs.LinuxVRChatSdkPatch.Worlds.Editor
 
             // Making sure that the paths are using forward slashes
             var bundleFilePath = ((string)__args[0]).Replace('\\', '/');
-            var pluginFilePath = ((string)__args[1]).Replace('\\', '/');
-            
+
             bundleFilePath = UnityWebRequest.EscapeURL(bundleFilePath).Replace("+", "%20");
-            pluginFilePath = UnityWebRequest.EscapeURL(pluginFilePath).Replace("+", "%20");
 
             var args = new StringBuilder();
             args.Append("run ");
             args.Append(vrcInstallPath);
             args.Append(' ');
-            
+
             args.Append("--url=create?roomId=");
                 args.Append(VRC.Tools.GetRandomDigits(10)); // Random roomId
                     args.Append("&hidden=true");
                         args.Append("&name=BuildAndRun");
                             args.Append("&url=file:///");
                                 args.Append(bundleFilePath);
-                                if (!string.IsNullOrEmpty(pluginFilePath))
-                                {
-                                    args.Append("&pluginUrl=file:///");
-                                    args.Append(pluginFilePath);
-                                }
-            
+
             args.Append(" --enable-debug-gui");
             args.Append(" --enable-sdk-log-levels");
             args.Append(" --enable-udon-debug-logging");
@@ -65,10 +58,10 @@ namespace BefuddledLabs.LinuxVRChatSdkPatch.Worlds.Editor
                 args.Append(" --no-vr");
             if (VRCSettings.WatchWorlds)
                 args.Append(" --watch-worlds");
-            
+
 
             var argsPathFixed = Regex.Replace(args.ToString(), @"file:[/\\]*", "file:///Z:/"); // The file we have is relative to / and not the "c drive" Z:/ is /
-            
+
             Debug.Log(Base.Editor.Base.GetSavedProtonPath() + argsPathFixed);
 
             var processStartInfo =
